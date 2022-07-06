6208c422f14ad811c7e6094c340a481483e9f6b5
Miguel Garcia <miguel.garcia@constantcontact.com>
2022-07-06 13:59:19 -0400

    - created migration and seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $sql = "INSERT INTO `places` (`name`, visited) VALUES ('Berlin',0),('Budapest',0),('Cincinnati',1),('Denver',0),('Helsinki',0),('Lisbon',0),('Moscow',1),('Nairobi',0),('Oslo',1),('Rio',0),('Tokyo',0);";
+        DB::unprepared($sql);
+        $this->command->info('Places table seeded!');
     }
 }
