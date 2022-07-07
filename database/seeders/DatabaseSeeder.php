5c8ae11dc3a4dc9b137844191394503b2f473c99
Jerry Ablan <jerry.ablan@sharpspring.com>
2022-07-07 12:04:25 -0400

    Current work checkpoint. See diff for details.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $sql = "INSERT INTO `places` (`name`, visited) VALUES ('Berlin',0),('Budapest',0),('Cincinnati',1),('Denver',0),('Helsinki',0),('Lisbon',0),('Moscow',1),('Nairobi',0),('Oslo',1),('Rio',0),('Tokyo',0);";
-        DB::unprepared($sql);
-        $this->command->info('Places table seeded!');
+        $this->call([UsersTableSeeder::class]);
     }
 }
